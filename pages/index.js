c531e4eceb977b43ba522f0712f1f221047dfbed
Adeola Adeyemo <adeola@formpl.us>
2020-12-30 16:00:54 +0100

    PORT1 - Created portfolio repository and setup Next, Tailwind, Prettier and Eslint
    Signed-off-by: Adeola Adeyemo <adeola@formpl.us>

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Portfolio of Adeola Adeyemo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="my-8 text-center text-2xl">New portfolio - Coming soon</h1>
+      </main>
+    </>
+  );
+}

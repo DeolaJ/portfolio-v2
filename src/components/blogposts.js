b210a9ae28c94d32c76c2a8ca69d41708c422155
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2021-03-20 09:41:58 +0100

    Updated blogpost links
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- src/components/blogposts.js
+++ src/components/blogposts.js
@@ -7,25 +7,25 @@ const BlogPosts = () => {
       id: 1,
       title: 'How to Create a Simple Cookie Consent Pop-up for your Website',
       link:
-        'https://blog.formpl.us/how-to-create-a-simple-cookie-consent-pop-up-for-your-website-dad17a174b60?source=your_stories_page---------------------------',
+        'https://blog.formpl.us/how-to-create-a-simple-cookie-consent-pop-up-for-your-website-dad17a174b60',
     },
     {
       id: 2,
       title: 'Creating A Simple Collapsible Sidebar with JavaScript (No Framework Required)',
       link:
-        'https://blog.formpl.us/creating-a-simple-collapsible-sidebar-with-javascript-no-framework-required-af9f8265d156?source=your_stories_page---------------------------',
+        'https://blog.formpl.us/creating-a-simple-collapsible-sidebar-with-javascript-no-framework-required-af9f8265d156',
     },
     {
       id: 3,
       title: 'How to create an Image Uploader using React and Cloudinary',
       link:
-        'https://medium.com/frontend-digest/how-to-create-an-image-uploader-using-react-and-cloudinary-5f07959723c4?source=your_stories_page---------------------------',
+        'https://medium.com/frontend-digest/how-to-create-an-image-uploader-using-react-and-cloudinary-5f07959723c4',
     },
     {
       id: 4,
       title: 'Twin.macro Tutorial for Beginners: Styling with Tailwind CSS and Emotion in React',
       link:
-        'https://blog.formpl.us/twin-macro-tutorial-for-beginners-styling-with-tailwind-css-and-emotion-in-react-5228c819d713?source=your_stories_page-------------------------------------',
+        'https://blog.formpl.us/twin-macro-tutorial-for-beginners-styling-with-tailwind-css-and-emotion-in-react-5228c819d713',
     },
     {
       id: 5,

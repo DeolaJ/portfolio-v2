bd11e7a79ae5932cd4aa38159ee469ff1a14467d
Adeola J. Adeyemo <adeola.adeyemoj@yahoo.com>
2022-01-24 10:42:15 +0100

    update: add thank you message to birthday page

--- src/components/pages/birthday-party.tsx
+++ src/components/pages/birthday-party.tsx
@@ -26,9 +26,7 @@ const BirthdayParty: FC = () => {
     'Omolara',
     'Tobi (Lala)',
     'Olufolarin',
-    'Lateef',
     'Mary && Ayobami',
-    'Adedayo',
     'Ibrahim',
     'Betty',
     'Mariam',
@@ -40,12 +38,11 @@ const BirthdayParty: FC = () => {
     'William',
     'Hannah',
     'Oluwaseun',
-    'Oluwole',
+    'Oluwole && Amaka',
     'Zainab Lawal',
     'Zainab Sowemimo',
     'Onyinye Mbieli',
-    'Precious Chilaka',
-    'Bassey',
+    'Yusuf',
     'Ebuka Moh',
     'Rachel',
     'Ismail Shofoluwe',
@@ -56,7 +53,7 @@ const BirthdayParty: FC = () => {
     <section>
       <Header
         title="12PM - 7PM, 22nd Jan. 2022"
-        subtitle="Birthday Parry"
+        subtitle="Thank you (Birthday Parry)"
         roles="Venue: 32 Adeyeye Street, Millennium Estate, Gbagada, Lagos (Opposite 25A)"
         imageLink="https://res.cloudinary.com/dzpntisxj/image/upload/v1637828679/digital-art/93271852-FBA7-49A0-A5DF-44DC966D7AAB_ndtarh.png"
         imgWidth={350 * 1.25}
@@ -64,32 +61,20 @@ const BirthdayParty: FC = () => {
       />
 
       <AboutTextWrapper className="py-20 md:py-32 lg:py-52 text-center bg-opacity-30 bg-custom-tea -mt-12 md:-mt-20">
-        <SectionTitle>About</SectionTitle>
-        <SectionTitleSub>A little more details</SectionTitleSub>
+        <SectionTitle>Appreciation</SectionTitle>
+        <SectionTitleSub>Thank you</SectionTitleSub>
         <article className="my-4">
-          <p className="text-base">Majorly music, networking, fun games, food and drinks.</p>
+          <p className="text-base">Thank you for attending my birthday party. You made my day and I truly hope it was fun for you</p>
           <p className="text-base">
-            Feel free to bring games (board, cards, if you have), drinks, snacks etc.. More is
-            always welcome. Reach out if you have questions about this
+            If you connected with anyone at the party, and want to get their contact details, feel free to reach out to me
           </p>
         </article>
       </AboutTextWrapper>
 
       <Section className="text-center">
-        <SectionTitle>Password</SectionTitle>
-        <SectionTitleSub>Millenium Estate Gate Password</SectionTitleSub>
-        <article className="my-4">
-          <p className="text-base">
-            The password is ZD32 HASH. To reach me, call +2347062972181 (main) or +2348123857352
-          </p>
-        </article>
-      </Section>
-
-      <Section className="text-center">
         <SectionTitle>Attendees</SectionTitle>
         <SectionTitleSub>
-          List of people that will be around. So attendees that know each other can move together if
-          possible
+          List of people that attendees that came around
         </SectionTitleSub>
         <article className="my-8">
           <ul className="flex flex-wrap items-center justify-center">
